3375f9b710d4b37be4d0735eeef17f4bb3c3b1d2
Adam Kaewell <adam.kaewell@gmail.com>
2019-12-27 10:50:41 -0500

    4.7 bigger-steps

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,13 +3,13 @@ class Bottles
     case number
     when 0
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
-      "no more bottles of beer.\n" +
-      "Go to the store and buy some more, " +
+      "#{quantity(number)} #{container(number)} of beer.\n" +
+      "#{action(number)}, " +
       "99 bottles of beer on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
-      "#{number} #{container(number)} of beer.\n" +
-      "Take #{pronoun(number)} down and pass it around, " +
+      "#{quantity(number)} #{container(number)} of beer.\n" +
+      "#{action(number)}, " +
       "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
     end
   end
@@ -43,6 +43,14 @@ class Bottles
       "no more"
     else
       number.to_s
+    end
+  end
+
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(number)} down and pass it around"
     end
   end
 end
